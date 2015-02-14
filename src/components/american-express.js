568c34adcf2784531aad778dd8193fa5958fadd8
Kier Borromeo <seraphipod@gmail.com>
2015-02-14 14:33:55 +0800

    Added american-express validator

---
+++ src/components/american-express.js
@@ -0,0 +1,9 @@
+var START = 3;
+var SECOND = [4, 7];
+var LENGTH = 15;
+
+module.exports = function(input) {
+  return input.length == 15 &&
+    input.charAt(0) == START &&
+    SECOND.indexOf(charAt(1));
+}
