d014a8248bc129122dafc1f0e28d283c9d99fdd0
Kier Borromeo <seraphipod@gmail.com>
2015-02-14 15:30:53 +0800

    Fixed unreferenced (typo) `input` variable

--- src/components/CreditCardVerifier/american-express.js
+++ src/components/CreditCardVerifier/american-express.js
@@ -5,5 +5,5 @@ var LENGTH = 15;
 module.exports = function(input) {
   return input.length == 15 &&
     input.charAt(0) == START &&
-    SECOND.indexOf(charAt(1));
+    SECOND.indexOf(input.charAt(1));
 }
