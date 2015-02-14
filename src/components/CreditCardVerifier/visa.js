9363db55abd73c9b7492aa6d7439d7ed63987b0b
Kier Borromeo <seraphipod@gmail.com>
2015-02-14 15:17:06 +0800

    Fixed condition for max number

--- src/components/CreditCardVerifier/visa.js
+++ src/components/CreditCardVerifier/visa.js
@@ -5,5 +5,5 @@ var START = '4';
 module.exports = function(input) {
   var length = input.length;
   
-  return (length >= MIN && length < MAX) && input.charAt(0) == START;
+  return (length >= MIN && length <= MAX) && input.charAt(0) == START;
 }
