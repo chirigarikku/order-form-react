ca2766eba8368d7869d6cae1e9415ce6a8fd8206
Kier Borromeo <seraphipod@gmail.com>
2015-02-14 15:25:35 +0800

    Fixed another set of unclosed parenthesises

--- src/components/CreditCardVerifier/__tests__/american-express-test.js
+++ src/components/CreditCardVerifier/__tests__/american-express-test.js
@@ -41,19 +41,19 @@ describe('american-express card verifier', function() {
     });
     
     it('should succeed when starting number is 3', function() {
-      expect(validator('341231231231231').toBe(true);
+      expect(validator('341231231231231')).toBe(true);
     });
   });
   
   describe('all-together now', function() {
     it('will suceed when starting number is 3, second number is either 4 or 7, and has length of 15', function() {
-      expect(validator('341231231231232').toBe(true);
+      expect(validator('341231231231232')).toBe(true);
     });
     
     it('will error when starting number is not 3, second number is either 4 or 7, and has length of 15', function() {
-      expect(validator('441231231231232').toBe(false);
-      expect(validator('541231231231232').toBe(false);
-      expect(validator('241231231231232').toBe(false);
+      expect(validator('441231231231232')).toBe(false);
+      expect(validator('541231231231232')).toBe(false);
+      expect(validator('241231231231232')).toBe(false);
     });
   });
 });
