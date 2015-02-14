b580071c40c7dba93763305d8a35f3d980ff8c20
Kier Borromeo <seraphipod@gmail.com>
2015-02-14 14:29:36 +0800

    Added validator for Visa.

---
+++ src/components/visa.js
@@ -0,0 +1,9 @@
+var MIN = 13;
+var MAX = 16;
+var START = '4';
+
+module.exports = function(input) {
+  var length = input.length;
+  
+  return (length >= MIN && length < MAX) && input.charAt(0) == START;
+}
